0fdcfca75bbba3f23c01e17389dbe93ac86e558d
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-04-16 16:49:59 +0300

    Catch events which coming from content_script

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,27 @@
-console.log('Hello World');
+import { type RequestInterFace, MESSAGES } from './interfaces';
+
+chrome.runtime.onMessage.addListener(
+  (request: RequestInterFace, sender, sendResponse) => {
+    const { message, payload } = request;
+
+    switch (message) {
+      case MESSAGES.ADD_NEW_BOOKMARK:
+        console.log('New Bookmark will add: ', payload);
+        break;
+
+      default:
+        break;
+    }
+
+    return true;
+  },
+);
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url !== null) {
+    chrome.tabs.sendMessage(tabId, {
+      message: MESSAGES.URL_CHANGED,
+      payload: { URL: tab.url, title: tab.title },
+    });
+  }
+});
